d48c10553e1fb80f39e388c1fe71a1e80c17af0a
Hal Henke <hal.henke@zoorepublic.com>
2015-07-31 14:18:54 +1000

    build assets

---
+++ public/progress.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var progressMeter = document.querySelector('.lightbox .progress .progress-meter');
+var progressMessage = document.querySelector('.lightbox .progress .progress-message');
+var resetButton = document.querySelector('.lightbox .reset button');
+var dismissButton = document.querySelector('.lightbox .header .dismiss');
+var progress = 0;
+
+// For IE 8
+$('.lightbox').attr('opacity', 0);
+
+resetButton.onclick = function () {
+  progress = 0;
+};
+
+dismissButton.onclick = function () {
+  $('.lightbox').animate({
+    opacity: 0
+  }, 1000, "linear", function () {});
+};
+
+var progressUpdate = function progressUpdate() {
+  if (progress <= 100) {
+    progressMeter.style['background-color'] = 'blue';
+    progressMeter.style.width = progress + '%';
+    progressMessage.innerHTML = 'Progress ' + progress + '%';
+    progress += 1;
+  } else {
+    progressMessage.innerHTML = 'This task is ' + (progress - 1) + '% complete. <span class="mega-octicon octicon-check"></span>';
+    progressMeter.style['background-color'] = 'green';
+  }
+};
+
+window.onload = function () {
+  $.getJSON("public/data.json", function (data) {
+    $('.lightbox').animate({
+      opacity: 1
+    }, 3000, "linear", function () {
+      window.setInterval(progressUpdate, data.speed);
+    });
+  });
+};
+//# sourceMappingURL=progress.js.map
